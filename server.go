ab174c2bfa8ce3594e3cd36e34cf20a5683fc123
Ike <iasomugha2010@my.fit.edu>
2025-05-14 23:46:15 -0700

    In-memory KV Store without persistence

--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -12,6 +14,8 @@ import (
 type ErrorMessage struct {
 	Error string `json:"error"`
 }
+
+var data = map[string]string{}
 
 func main() {
 	// Get port from env variables or set to 8080
@@ -24,13 +28,78 @@ func main() {
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		b, err := json.Marshal(map[string]string{"hello": "world"})
+		JSON(w, map[string]string{"hello": "world"})
+	})
+
+	r.Get("/key/{key}", func(w http.ResponseWriter, r *http.Request) {
+		key := chi.URLParam(r, "key")
+
+		data, err := Get(r.Context(), key)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
-			b, _ = json.Marshal(ErrorMessage{Error: err.Error()})
+			JSON(w, map[string]string{"error": err.Error()})
+			return
 		}
-		w.Write(b)
+
+		w.Write([]byte(data))
+	})
+
+	r.Delete("/key/{key}", func(w http.ResponseWriter, r *http.Request) {
+		key := chi.URLParam(r, "key")
+
+		err := Delete(r.Context(), key)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			JSON(w, map[string]string{"error": err.Error()})
+			return
+		}
+
+		JSON(w, map[string]string{"status": "success"})
+	})
+
+	r.Post("/key/{key}", func(w http.ResponseWriter, r *http.Request) {
+		key := chi.URLParam(r, "key")
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			JSON(w, map[string]string{"error": err.Error()})
+			return
+		}
+
+		err = Set(r.Context(), key, string(body))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			JSON(w, map[string]string{"error": err.Error()})
+			return
+		}
+
+		JSON(w, map[string]string{"status": "success"})
 	})
 
 	log.Fatal(http.ListenAndServe(":"+port, r))
+}
+
+func Set(ctx context.Context, key, value string) error {
+	data[key] = value
+	return nil
+}
+func Get(ctx context.Context, key string) (string, error) {
+	return data[key], nil
+}
+
+func Delete(ctx context.Context, key string) error {
+	delete(data, key)
+	return nil
+}
+
+// JSON encodes data to json and writes to the http response
+func JSON(w http.ResponseWriter, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	b, err := json.Marshal(data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		b, _ = json.Marshal(ErrorMessage{Error: err.Error()})
+	}
+
+	w.Write(b)
 }
